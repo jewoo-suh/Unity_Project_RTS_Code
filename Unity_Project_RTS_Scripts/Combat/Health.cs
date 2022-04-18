322e9b12ea99d5dea4606775770add106bc416b8
jewoo-suh <92768504+jewoo-suh@users.noreply.github.com>
2022-04-18 15:27:43 +0100

    Add files via upload

---
+++ Unity_Project_RTS_Scripts/Combat/Health.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar(hook = nameof(HandleHealthUpdated))]
+    private int currentHealth;
+
+    public event Action<int, int> ClientOnHealthUpdated;
+
+    public event Action ServerOnDie;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        currentHealth = maxHealth;
+
+        UnitBase.ServerOnPlayerDie += ServerHandlePlayerDie;
+    }
+
+    public override void OnStopServer()
+    {
+        UnitBase.ServerOnPlayerDie -= ServerHandlePlayerDie;
+    }
+
+    [Server]
+    private void ServerHandlePlayerDie(int connectionId)
+    {
+        if(connectionToClient.connectionId != connectionId) { return; }
+
+        DealDamage(currentHealth);
+    }
+
+    [Server]
+    public void DealDamage(int damageAmount)
+    {
+        if(currentHealth == 0) { return; }
+
+        currentHealth = Mathf.Max(currentHealth - damageAmount, 0);
+
+        if (currentHealth != 0) { return; }
+
+        ServerOnDie?.Invoke();
+
+
+    }
+
+    #endregion
+
+    #region Client
+
+    private void HandleHealthUpdated(int oldHealth, int newHealth)
+    {
+        ClientOnHealthUpdated?.Invoke(newHealth, maxHealth);
+    }
+
+    #endregion
+}
